1e4244347d8b77f89fa8f620dc0ebfed6b809985
Jing Qian <78173171+sulphemi@users.noreply.github.com>
2024-10-08 00:33:35 -0400

    Update week01.py

--- week01/week01.py
+++ week01/week01.py
@@ -8,7 +8,7 @@ for i in ['tiger', 'elephant', 'monkey', 'zebra', 'panther']:
 
 # 3. Write a program that iterates from 1 to 20, printing each number and whether it's odd or even.
 for i in range(1, 20 + 1):
-    print(f"{i} {'odd' if i & 1 else 'even'}");
+    print(f"{i} {'odd' if i % 2 else 'even'}");
 
 # 4. Write a function sum_of_integers(a, b) that takes two integers as input from the user and returns their sum.
 def sum_of_integers(a, b):
